83993aa27495bd48dd9bf7cc5b7597dd2bf57dde
Cuan <idgr@tutanota.com>
2022-03-16 03:34:33 -0400

    Organized repo for packaging

---
+++ package.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# This script packages rg2sc (from source) for Arch Linux.
+
+echo -e '\n\nThe value entered in this field will only affect the version reported to the package manager. The latest source is used regardless.\n'
+read -r -p "Version number: " version
+read -r -p "Revision number: " revision
+
+# Archive creation
+echo -e '\nPackaging as generic...\n'
+mkdir -p packages/archtemp/usr
+cp -r bin packages/archtemp/usr/
+cp -r share packages/archtemp/usr/
+tar -C packages/archtemp -cvf packages/sshyp-"$version".tar.xz usr/
+rm -rf packages/archtemp
+sha512="$(sha512sum packages/sshyp-"$version".tar.xz | awk '{print $1;}')"
+echo -e "\nsha512 sum:\n$sha512"
+echo -e "\nGeneric packaging complete.\n"
+
+# PKGBUILD creation
+echo -e '\nGenerating PKGBUILD...'
+echo "# Maintainer: Randall Winkhart <idgr at tutanota dot com>
+
+pkgname=rg2sc
+pkgver="$version"
+pkgrel="$revision"
+pkgdesc='Converts ReplayGain ID3 tags to Apple Sound Check tags'
+url='https://github.com/rwinkhart/rg2sc'
+arch=('any')
+license=('GPL2')
+depends=(python python-mutagen)
+
+source=(\""$source"\")
+sha512sums=('"$sha512"')
+
+package() {
+
+    tar xf sshyp-"\"\$pkgver\"".tar.xz -C "\"\${pkgdir}\""
+
+}
+" > packages/PKGBUILD
+    echo -e "\nPKGBUILD generated.\n"
